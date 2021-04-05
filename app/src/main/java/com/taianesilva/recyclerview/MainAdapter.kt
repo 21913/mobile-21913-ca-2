5e92cbc631f266abf85b56e0200bb2a4767bdb9b
taivieras <taiveiras@gmail.com>
2021-04-05 14:10:18 +0100

    2nd commit

---
+++ app/src/main/java/com/taianesilva/recyclerview/MainAdapter.kt
@@ -0,0 +1,72 @@
+package com.androidluana.kotlinyoutubelbta
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.taianesilva.recyclerview.CourseDetailActivity
+import com.taianesilva.recyclerview.HomeFeed
+import com.taianesilva.recyclerview.R
+import com.taianesilva.recyclerview.Video
+import kotlinx.android.synthetic.main.video_row.view.*
+
+
+class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
+
+    val videoTitles = listOf<String>("First title", "Second", "3rd", "MOOOOORE TITLE")
+
+    // number of items
+    override fun getItemCount(): Int {
+        return homeFeed.videos.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+
+//        val videoTitle = videoTitles.get(position)
+        val video = homeFeed.videos.get(position)
+        holder.view.textView_video_title.text = video.name
+
+        holder.view.textView_channel_name.text = video.channel.name + "  •  " + "20k Views\n4 days ago"
+
+        val thumbnailImageView = holder.view.imageView_video_thumbnail
+        Picasso.with(holder.view.context).load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView = holder.view.imageView_channel_profile
+        Picasso.with(holder.view.context).load(video.channel.profileImageUrl).into(channelProfileImageView)
+
+        holder?.video = video
+
+    }
+}
+
+class CustomViewHolder(val view: View, var video: Video? = null) : RecyclerView.ViewHolder(view) {
+
+    companion object{
+
+        val VIDEO_TITLE_KEY = "VIDEO_TITLE"
+        val VIDEO_ID_KEY = "VIDEO_ID"
+    }
+
+    init {
+        view.setOnClickListener {
+            println("TEST")
+
+
+            val intent = Intent(view.context, CourseDetailActivity::class.java)
+
+            intent.putExtra(VIDEO_TITLE_KEY, video?.name)
+            intent.putExtra(VIDEO_ID_KEY, video?.id)
+
+            view.context.startActivity(intent)
+        }
+    }
+}
